b9abd5f55be8eac48be9398fafa135374c530478
Bubbelbad <victor.ivarson@hotmail.se>
2024-05-15 20:09:11 +0200

    Added ShoppingCartServiceTest to unit test

---
+++ ECommerceTest/ShoppingCartServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Models;
+using ECommerce.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceTest
+{
+    public class ShoppingCartServiceTest
+    {
+        [Fact]
+        public void GetShoppingCartList_TestNotNull()
+        {
+            //Arrange
+            ShoppingCartService service = new ShoppingCartService();
+
+            //Act
+            List<CartItem> cartItems = service.GetShoppingCartList();
+
+            //Assert
+            Assert.NotNull(cartItems); 
+        }
+    }
+}
